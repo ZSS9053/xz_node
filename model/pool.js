fc50bb6df0399c1b5591405c2bb0b3e1f13a0e3c
ZhuShuangshuang <635853904@qq.com>
2017-11-15 19:26:27 +0800

    update

--- model/pool.js
+++ model/pool.js
@@ -3,10 +3,10 @@
 * */
 const mysql=require('mysql');
 let pool=mysql.createPool({
-  host:'127.0.0.1',
-  user:'root',
-  password:'',
-  database:'xz',
+  host:'w.rdc.sae.sina.com.cn',
+  user:'mk2my4xk4y',
+  password:'00kjmkw4323l55xyllwlz1jh423wlhxz4y5jl331',
+  database:'app_notestore',
   port:'3306',
   connectionLimit:10
 });
